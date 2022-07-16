759b979207a0745693d200a5a77bd2eec91ccd6c
Dimitrios Taskoudis <detaskou@office365.auth.gr>
2022-07-17 01:02:42 +0300

    end of course JOKES

--- Jokes/Controllers/JokesController.cs
+++ Jokes/Controllers/JokesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Jokes.Data;
 using Jokes.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Jokes.Controllers
 {
@@ -33,10 +34,10 @@ namespace Jokes.Controllers
         }
 
         // Post: Jokes/ShowSearchResults
-        public string ShowSearchResults(string SearchPhrase)
+        public async Task<IActionResult> ShowSearchResults(String SearchPhrase)
         {
-            return "You entered " + SearchPhrase;
-        } 
+            return View("Index", await _context.Joke.Where( j => j.JokeQuestion.Contains(SearchPhrase)).ToListAsync());
+        }
 
         // GET: Jokes/Details/5
         public async Task<IActionResult> Details(int? id)
@@ -57,6 +58,7 @@ namespace Jokes.Controllers
         }
 
         // GET: Jokes/Create
+        [Authorize]
         public IActionResult Create()
         {
             return View();
@@ -65,6 +67,7 @@ namespace Jokes.Controllers
         // POST: Jokes/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,JokeQuestion,JokeAnswer")] Joke joke)
@@ -79,6 +82,7 @@ namespace Jokes.Controllers
         }
 
         // GET: Jokes/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null || _context.Joke == null)
@@ -97,6 +101,7 @@ namespace Jokes.Controllers
         // POST: Jokes/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,JokeQuestion,JokeAnswer")] Joke joke)
@@ -130,6 +135,7 @@ namespace Jokes.Controllers
         }
 
         // GET: Jokes/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null || _context.Joke == null)
@@ -148,6 +154,7 @@ namespace Jokes.Controllers
         }
 
         // POST: Jokes/Delete/5
+        [Authorize]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
