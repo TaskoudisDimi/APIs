86d2c590fe30a24260b0fece7fd942f5d1571a99
Taskoudis Dimitris <63710755+Taskoudis-Dimi@users.noreply.github.com>
2022-09-09 12:09:03 +0300

    Delete Test directory

--- Test/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Test.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
