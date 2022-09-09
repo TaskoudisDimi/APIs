afb488c887f0c1712a784cb67daeba25efa9fc2e
Taskoudis Dimitris <63710755+Taskoudis-Dimi@users.noreply.github.com>
2022-09-09 12:19:09 +0300

    Delete WebAPIAuthentication directory

--- WebAPIAuthentication/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace WebAPIAuthentication.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
