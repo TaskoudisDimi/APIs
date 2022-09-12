f42fad143b3f9e6d1eb27b72036e419d826e79a4
Taskoudis Dimitris <taskoudisdimitris@gmail.com>
2022-09-12 17:27:22 +0300

    Comments

---
+++ SuperMarketAPI/Controllers/CreateCustomResultType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace FullStackAppAPI.Controllers
+{
+    public class CreateCustomResultType : IHttpActionResult
+    {
+        string _value;
+        HttpRequestMessage _request;
+
+
+        public CreateCustomResultType(string value, HttpRequestMessage request)
+        {
+            _value = value;
+            _request = request;
+
+        }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage()
+            {
+                Content = new StringContent(_value),
+                RequestMessage = _request
+            };
+
+            return Task.FromResult(response);
+
+        }
+
+
+        
+    }
+}
---
+++ SuperMarketAPI/Controllers/LogAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace FullStackAppAPI.Controllers
+{
+    public class LogAttribute : Attribute, IActionFilter
+    {
+        //In the bellow example, deriving from Attribute class makes it an attribute and implementing IActionFilter makes LogAttribute class
+        //as action filter.
+        public LogAttribute()
+        {
+
+        }
+
+        public Task<HttpResponseMessage> ExecuteActionFilterAsync(HttpActionContext actionContext, CancellationToken cancellationToken, Func<Task<HttpResponseMessage>> continuation)
+        {
+            Trace.WriteLine(string.Format("Action Method {0} executing at {1}", actionContext.ActionDescriptor.ActionName, DateTime.Now.ToShortDateString()), "Web API Logs");
+
+            var result = continuation();
+
+            result.Wait();
+
+            Trace.WriteLine(string.Format("Action Method {0} executed at {1}", actionContext.ActionDescriptor.ActionName, DateTime.Now.ToShortDateString()), "Web API Logs");
+
+            return result;
+        }
+
+
+        public bool AllowMultiple
+        {
+            get { return true; }
+        }
+    }
+}
