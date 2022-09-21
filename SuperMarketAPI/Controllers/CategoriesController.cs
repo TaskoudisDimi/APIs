6106c0eec019f016ea1e1568662861fd8a5f24c7
Dimitrios Taskoudis <taskoudisdimitris@gmail.com>
2022-09-22 00:06:44 +0300

    create class for dataaccess, create categories controller

--- SuperMarketAPI/Controllers/CategoriesController.cs
+++ SuperMarketAPI/Controllers/CategoriesController.cs
@@ -2,16 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
+using System.Web.Http;
+using System.Data;
+using System.Net.Http;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Net;
+using FullStackAppAPI.Models;
 
 namespace FullStackAppAPI.Controllers
 {
-    public class CategoriesController : Controller
+    public class CategoriesController : ApiController
     {
-        // GET: Categories
-        public ActionResult Index()
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("api/categories")]
+        public HttpResponseMessage GetAllProducts()
         {
-            return View();
+            string query = @"Select * From CategoriesTbl";
+
+            DataTable table = new DataTable();
+
+            using (var con = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["smarketdb"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var adapter = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                adapter.Fill(table);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("api/categories/id")]
+        public HttpResponseMessage GetValue(int Id)
+        {
+            string query = @"Select * From CategoriesTbl where Catid=" + Id + "";
+
+            DataTable table = new DataTable();
+
+            using (var con = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["smarketdb"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var adapter = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                adapter.Fill(table);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, table);
         }
     }
 }
--- SuperMarketAPI/Models/Categories.cs
+++ SuperMarketAPI/Models/Categories.cs
@@ -7,5 +7,22 @@ namespace FullStackAppAPI.Models
 {
     public class Categories
     {
+
+        public int Catid { get; set; }
+        public string CatName { get; set;}
+        public string CatDesc { get; set; }
+        public DateTime Date { get; set; }  
+
+
+
+
     }
+
+
+
+
+
+
+
+
 }
