8ef166ecd14ec911bd3c56ad5568d8d6cd8740b6
Dimitris_Taskoudis <taskoudisdimitris@gmail.com>
2022-11-25 17:30:02 +0200

    Test ControllerBase

--- Test/Models/Products.cs
+++ Test/Models/Products.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace FullStackAppAPI.Models
+﻿namespace Test.Models
 {
     public class Products
     {
+
         public int ProdId { get; set; }
         public string ProdName { get; set; }
         public int ProdQty { get; set; }
         public int ProdPrice { get; set; }
         public string ProdCat { get; set; }
         public DateTime Date { get; set; }
-    
+
+
+
     }
 }
---
+++ TrackingAPI/Repository/MembersRepository.cs
@@ -0,0 +1,28 @@
+﻿using TrackingAPI.Interfaces;
+using TrackingAPI.Model;
+
+namespace TrackingAPI.Repository
+{
+    public class MembersRepository : IMember
+    {
+        List<Member> lisMembers = new List<Member>
+        {
+            new Member{MemberId=1, FirstName="Kirtesh", LastName="Shah", Address="Vadodara" },
+            new Member{MemberId=2, FirstName="Nitya", LastName="Shah", Address="Vadodara" },
+            new Member{MemberId=3, FirstName="Dilip", LastName="Shah", Address="Vadodara" },
+            new Member{MemberId=4, FirstName="Atul", LastName="Shah", Address="Vadodara" },
+            new Member{MemberId=5, FirstName="Swati", LastName="Shah", Address="Vadodara" },
+            new Member{MemberId=6, FirstName="Rashmi", LastName="Shah", Address="Vadodara" },
+        };
+        public List<Member> GetAllMember()
+        {
+            return lisMembers;
+        }
+
+        public Member GetMember(int id)
+        {
+            return lisMembers.FirstOrDefault(x => x.MemberId == id);
+        }
+
+    }
+}
